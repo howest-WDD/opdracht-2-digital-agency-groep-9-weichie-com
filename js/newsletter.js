1a6cc661f3b67b4049cc345ab779a8a425f11fcc
sammussly <sam.cornflakes1998@gmail.com>
2022-11-15 15:49:36 +0100

    add newsletter transitions

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,21 +1,49 @@
-let modal
+let modal;
+let sect;
+
+const inViewport = new IntersectionObserver(function (el) {
+  if (el[0].isIntersecting) {
+    modal.classList.add("c-newsletter__animatein");
+    console.log("yo");
+    modal.classList.remove("c-newsletterhidden");
+  }
+});
 
 const togglearrow = function () {
-    const arrow = document.querySelector(".js-newsletter-arrow");
-    body.classList.toggle("o-darkmode");
-  };
-  
-  const listenClose = function () {
-    const close = document.querySelector(".js-newsletter-close");
-    
-    close.addEventListener("click", function(){
-        modal.classList.add("c-newsletterhidden")
-    })
-  };
-  
-  const init = function () {
-    modal = document.querySelector(".js-newsletter")
-    listenClose();
-    togglearrow();
-  };
-  document.addEventListener("DOMContentLoaded", init);
+  const arrow = document.querySelector(".js-newsletter-arrow");
+  arrow.addEventListener("click", function () {
+    // modal.classList.remove("c-newswletter__animatein");
+    if (modal.classList.contains("c-newsletter__animatearrowout")) {
+      modal.classList.remove("c-newsletter__animatearrowout");
+      modal.classList.add("c-newsletter__animatearrowin");
+      console.log("its small so make big");
+    } else {
+      modal.classList.remove("c-newsletter__animatearrowin");
+      modal.classList.add("c-newsletter__animatearrowout");
+      console.log("its big so make small");
+    }
+    console.log("click arrow");
+  });
+};
+
+const listenClose = function () {
+  const close = document.querySelector(".js-newsletter-close");
+
+  close.addEventListener("click", function () {
+    // modal.classList.remove("c-newswletter__animatein");
+
+    modal.classList.add("c-newsletter__animateout");
+    modal.classList.remove("c-newsletter__animatein");
+    modal.classList.remove("c-newsletter__animatearrowin");
+    // modal.classList.add("c-newsletterhidden");
+  });
+};
+
+const init = function () {
+  sect = document.querySelector(".js-jobs");
+  inViewport.observe(sect);
+  modal = document.querySelector(".js-newsletter");
+  listenClose();
+  togglearrow();
+};
+document.addEventListener("DOMContentLoaded", init);
